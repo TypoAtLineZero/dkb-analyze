96fdcf11e49e2cb78ca6b2f851d97fd4e1440ab5
franz <fneumeister@in-technology.eu>
2024-12-10 12:24:55 +0100

    Adding CLI input field for user input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::Parser;
+use core::arch;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use log::{info, warn};
 use csv::ReaderBuilder;
-use std::path::PathBuf;
 
 mod categories;
 
@@ -13,11 +13,12 @@ use crate::categories::Expense;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long, required = false)]
-    path: Option<PathBuf>,
+    #[arg(short, long, required = false, default_value="")]
+    // file_path: Option<PathBuf>,
+    input: String,
 
-    #[arg(short, long, required = false)]
-    interval: Option<String>,
+    // #[arg(short, long, required = false)]
+    // interval: Option<String>,
 
     #[arg(short, long, required = false)]
     visualization: bool,
@@ -28,6 +29,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     info!("Starting DKB Analyze");
 
     let args = Cli::parse();
+    let mut file_path = args.input;
     // Define categories and their keywords
     let mut categories: HashMap<&str, Vec<&str>> = HashMap::new();
     categories.insert("Food", vec!["restaurant", "grocery", "cafe"]);
@@ -37,8 +39,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Totals for each category
     let mut category_totals: HashMap<&str, f64> = HashMap::new();
 
-    // Open the CSV file
-    let file = File::open("/home/franz/_devenv/private/dkb-analyze/src/spendings.csv")?;
+    match file_path.as_str() {
+        // "" => println!("Consider handing over your own csv, continuing with example data"),
+        "" => file_path.replace_range(.., "/home/franz/_devenv/private/dkb-analyze/src/spendings.csv"),
+        _ => println!("Found input file"),
+    }
+    println!("{}", file_path);
+
+    let file = File::open(file_path)?;
     let mut rdr = ReaderBuilder::new().has_headers(true).from_reader(file);
 
     // Iterate through each record
@@ -63,9 +71,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Print the totals for each category
-    info!("Category Totals:");
+    println!("Category Totals:");
     for (category, total) in &category_totals {
-        info!("{}: {:.2}", category, total);
+        println!("{}: {:.2}", category, total);
     }
 
     Ok(())
